41fedaf448dd2a6afa18be7c3a0e4d170f53dd0b
João Victor <74199862+joaofer19@users.noreply.github.com>
2022-06-03 10:45:35 -0300

    Update Program.java

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,17 @@
 package application;
 
-public class Program {
+import java.util.Scanner;
 
+import entities.Calculator;
+
+public class Program {
+	public  static void main(String[] args) {
+		String bin;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number in binary");
+		bin = sc.nextLine();
+		Calculator calc = new Calculator(bin);
+		calc.bin2dec(bin);
+		System.out.println("Decimal : " + calc.getDecimal());
+	}
 }
